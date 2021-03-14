b8f79a72c430a9811243c8e3f75e1ec82588030f
Collin Lefeber <collin@vimeo.com>
2021-03-13 22:36:42 -0500

    add gzip

--- src/LocalCall.jsx
+++ src/LocalCall.jsx
@@ -1,20 +1,29 @@
 import { Component } from 'inferno';
+import * as Pako from 'pako';
+
+const USER_TYPE_START = Symbol('start')
+const USER_TYPE_RECV = Symbol('recv')
 
 class LocalCall extends Component {
     constructor(props) {
         super(props);
         this.conn = null;
         this.state = {
+            user_type: null,
+            connectBlob: null,
+
             started: false,
             videoSrc: null,
             offer: null,
             ice: [],
         };
+
+        this.startCall = this._startCall.bind(this);
     }
 
     async _startCall() {
         this.setState({ started: true })
-        console.log('Starting', this);
+
         this.conn = new RTCPeerConnection({
             iceServers: [
                 {
@@ -50,19 +59,20 @@ class LocalCall extends Component {
         };
 
         let stream;
-        // stream = await navigator.mediaDevices.getUserMedia({audio: true, video: true})
         stream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
 
         stream.getTracks().forEach(track => this.conn.addTrack(track, stream));
         // TODO this doesn't work
         this.setState({ videoSrc: stream });
-
         // TODO inferno way of doing this?
         document.querySelector('#local_video').srcObject = stream;
 
-        const offer = await this.conn.createOffer()
-        this.conn.setLocalDescription(offer);
-        this.setState({offer: offer})
+        // user starting the call and creating the offer
+        if (this.state.user_type === USER_TYPE_START && !this.state.offer) {
+            const offer = await this.conn.createOffer()
+            this.conn.setLocalDescription(offer);
+            this.setState({offer: offer})
+        }
     }
 
     async _updateRemote() {
@@ -83,27 +93,57 @@ class LocalCall extends Component {
     }
 
     render() {
-        const connectData = {
-            offer: this.state.offer,
-            ice: this.state.ice,
-        };
+        if (!this.state.user_type) {
+            return (
+                <div>
+                    <p>Are you starting or receiving a call?</p>
+                    <button onclick={() => this.setState({ user_type: USER_TYPE_START })}>
+                        Starter
+                    </button>
+                    <br />
+                    <button onclick={() => this.setState({ user_type: USER_TYPE_RECV })}>
+                        Receiver
+                    </button>
+                </div>
+            )
+        }
 
-        const connectBlob = JSON.stringify(connectData);
+        if (this.state.user_type === USER_TYPE_START) {
+            if (this.state.offer && this.state.ice.length > 0) {
+                if (!this.state.connectBlob) {
+                    const connectData = {
+                        offer: this.state.offer,
+                        ice: this.state.ice,
+                    };
 
-        console.log(connectData);
+                    let connectBlob = JSON.stringify(connectData);
+                    connectBlob = Pako.deflate(JSON.stringify(connectBlob));
+                    connectBlob = window.btoa(connectBlob);
+                    this.setState({connectBlob});
+                }
+            }
+
+            return (
+                <div>
+                    <h2 class="h2">Local Call</h2>
+                    <video id="local_video" controls autoplay style={{width: '400px'}}></video>
+                    <br />
+                    <input type="button" value="Start capture"
+                        onclick={this.startCall} />
+                    <br />
+                    {this.state.connectBlob && <div>
+                        <p>Send this too your peer</p>
+                        <textarea>{this.state.connectBlob}</textarea>
+                    </div>}
+                </div>
+            )
+        }
+
         return (
             <div>
                 <div>
-                    <h2 class="h2">Local Call</h2>
-                    <video id="local_video" controls autoplay style={{width: '600px'}}></video>
-                    <br />
-                    <input type="button" value="Start Call" disabled={this.state.started} onclick={this._startCall.bind(this)} />
-                    <br />
-                    <textarea>{connectBlob}</textarea>
-                </div>
-                <div>
                     <h2 class="h2">Remote Call</h2>
-                    <video id="remote_video" controls autoplay style={{width: '600px'}}></video>
+                    <video id="remote_video" controls autoplay style={{width: '400px'}}></video>
                     <br />
                     <input type="button" value="Connect Remote" onclick={this._updateRemote.bind(this)} />
                     <br />
